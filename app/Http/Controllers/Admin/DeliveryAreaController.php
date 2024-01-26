11afdb2ce88eeab2e3b1d1d75acadd3cfad0bc17
BoberKrzysztof <k.bober@onet.pl>
2024-01-26 11:27:34 +0100

    Delivery Area Admin Done

--- app/Http/Controllers/Admin/DeliveryAreaController.php
+++ app/Http/Controllers/Admin/DeliveryAreaController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 use App\DataTables\DeliveryAreaDataTable;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\DeliveryAreaCreateRequest;
+use App\Models\DeliveryArea;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class DeliveryAreaController extends Controller
 {
@@ -19,41 +23,54 @@ class DeliveryAreaController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create() : View
     {
-        //
+        return view('admin.delivery-area.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(DeliveryAreaCreateRequest $request) : RedirectResponse
     {
-        //
-    }
+        $area = new DeliveryArea();
+        $area->area_name = $request->area_name;
+        $area->min_delivery_time = $request->min_delivery_time;
+        $area->max_delivery_time = $request->max_delivery_time;
+        $area->delivery_fee = $request->delivery_fee;
+        $area->status = $request->status;
+        $area->save();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        toastr()->success('Created Successfully');
+
+        return to_route('admin.delivery-area.index');
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id) : View
     {
-        //
+        $area = DeliveryArea::findOrFail($id);
+        return view('admin.delivery-area.edit', compact('area'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(DeliveryAreaCreateRequest $request, string $id)
     {
-        //
+        $area = DeliveryArea::findOrFail($id);
+        $area->area_name = $request->area_name;
+        $area->min_delivery_time = $request->min_delivery_time;
+        $area->max_delivery_time = $request->max_delivery_time;
+        $area->delivery_fee = $request->delivery_fee;
+        $area->status = $request->status;
+        $area->save();
+
+        toastr()->success('Updated Successfully');
+
+        return to_route('admin.delivery-area.index');
     }
 
     /**
@@ -61,6 +78,11 @@ class DeliveryAreaController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            DeliveryArea::findOrFail($id)->delete();
+            return response(['status' => 'success', 'message' => 'Deleted Successfully!']);
+        } catch (\Exception $e) {
+            return response(['status' => 'error', 'message' => 'Something went wrong!']);
+        }
     }
 }
